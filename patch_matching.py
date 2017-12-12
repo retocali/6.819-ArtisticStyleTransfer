338e20c068e14e41a63415482e9e86fe61d51c00
Davis Tran <dvtran@mit.edu>
2017-12-12 00:33:04 -0500

    quadtree works without distance formula.

--- patch_matching.py
+++ patch_matching.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from sys import argv
 from sklearn.feature_extraction import image
+import time
 
 
 def patch_matching(source, target, patches=(12, 16), sigma=1. / 4):
@@ -69,48 +70,95 @@ def create_sub_patches(img, max_patch_size, min_patch_size):
                 patchworks[patch_size].append(img[i:i+patch_size, j:j+patch_size])
     return patchworks
 
-def quad_tree(input_img, style_img, omega=15, max_patch_size=100, min_patch_size = 5):
+def quad_tree(input_img, style_img, omega=10, max_patch_size=100, min_patch_size = 25):
     # Read images and convert to color space
     s = cv2.imread(style_img).astype("float32")
     t = cv2.imread(input_img).astype("float32")
 
-    # Split the images into patches
+    s = cv2.resize(s, (512, 512))
+    t = cv2.resize(t, (512, 512))
+
+    # Take measurements of image
     print("Dim:", s.shape, t.shape)
     t_height = t.shape[0]
     t_width = t.shape[1]
-
     t_height_regions = t_height // max_patch_size
     t_width_regions = t_width // max_patch_size
-    print("t_height_regions: ", t_height_regions)
-    print("t_width_regions: ", t_width_regions)
+
+    # Split the images into patches
     patch_s = create_sub_patches(s, max_patch_size, min_patch_size)
-    patch_t = flatten([np.vsplit(row, t_height_regions) for row in np.hsplit(t, t_width_regions)][0])
-     
+    patch_t = flatten([np.vsplit(row, t_height_regions) for row in np.hsplit(t, t_width_regions)])
+
+    t0 = time.time()
+    # Find patches that match most closely
     matches = []
-    print('patch_t length: ', len(patch_t))
+    counter = 0
+    step_display = 5
     for p_t in patch_t:
+        if counter % step_display == 0:
+            print('t = {}'.format(time.time()-t0))
+        counter+=1
         ti = p_t.shape[0]
-        p_s = min(patch_s[ti], key=lambda x: np.sum(np.subtract(x, p_t)**2))
-        d = np.sum(np.subtract(p_s, p_t)**2)
-        eta = np.var(p_t) + d
-        if False: #eta > omega and ti >= min_patch_size:
-            print('hi')
+        p_s = min(patch_s[ti], key=lambda x: (np.sum(np.subtract(x, p_t))**2)/(ti**2))
+        d = np.sum(np.subtract(p_s, p_t)**2)/(ti**2)
+        eta = np.std(p_t)# + d
+        print('std: ', np.std(p_t))
+        print('d: ', d)
+        print('ti: ', ti)
+        print('='*20)
+        # Split under this condition
+        if eta > omega and ti > min_patch_size:
+            matches.append(split(p_t, patch_s, omega, min_patch_size))
+        
+        # Otherwise just add it
         else:
+            print('eta: ', eta)
             matches.append(p_s)
 
     
-      # Concatenate the patches into a image
+    # Concatenate the patches into a image
     print("Concatenate")
-    rows = []
-    print('shape of matches[0]: ', (matches[0].shape))
-    print('length of matches: ', len(matches))
+    cols = []
     for x in range(t_width_regions):
-        rows.append(np.vstack(matches[t_height_regions * x:t_height_regions * (x + 1)]))
-    matches = np.hstack(rows)          
-
+        col = np.vstack(matches[t_height_regions * x:t_height_regions * (x + 1)])
+        cols.append(col)
+    matches = np.hstack(cols)   
     return matches
-    
 
+def split(patch, style_patches, omega, min_patch_size):
+    
+    # Base Case Check    
+    patch_size = patch.shape[0]
+    if patch_size < min_patch_size:
+        print('smol')
+        return np.full((patch_size, patch_size, 3), 0)
+    split_patch = flatten([np.vsplit(row, 2) for row in np.hsplit(patch, 2)])
+
+    # Find patches that match most closely
+    matches = []
+    ti = patch_size//2
+    print('ti splitted: ', ti)
+    for sub_patch in split_patch:
+        matching_patch = min(style_patches[ti], key=lambda x: (np.sum(np.subtract(x, sub_patch)**2))/(ti**2))
+        d = np.sum(np.subtract(matching_patch, sub_patch)**2)/(ti**2)
+        eta = np.std(sub_patch)# + d
+
+        # Split under this condition
+        if eta > omega and ti > min_patch_size:
+            matches.append(split(sub_patch, style_patches, omega, min_patch_size))
+
+        # Otherwise just add it
+        else:
+            matches.append(matching_patch)
+    
+    # Concatenate the patches into a image
+    cols = []
+    for x in range(2):
+        col = np.vstack(matches[2 * x:2 * (x + 1)])
+        cols.append(col)
+    combined_patch = np.hstack(cols)   
+
+    return combined_patch
 
 def rotate_items(array):
     rotated_items = []
@@ -131,8 +179,10 @@ if __name__ == "__main__":
         s = str(argv[1])
         t = str(argv[2])
         #im = patch_matching(s, t)
-        im = quad_tree(t, s)
+        
+        im = quad_tree(s, t, max_patch_size = 64, min_patch_size = 8)
         print(im.shape)
+        im = cv2.resize(im, (500, 400))
         whole = np.hstack((cv2.imread(s) / 255, cv2.imread(t) / 255, im / 255))
         cv2.namedWindow("results", cv2.WINDOW_NORMAL)
         cv2.imshow('results', whole)
